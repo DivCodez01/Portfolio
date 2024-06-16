66a16cf48815c664535402c4fea18059987a3429
divin-gakiza <divin.gakiza01@gmail.com>
2024-06-16 20:05:31 +0200

    left details

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const nickName = document.getElementById('my-name');
 const symbol = document.querySelector('em');
 const aboutMe = document.getElementsByTagName('li');
 
+const leftBackImage = document.querySelector('.leftSide-container img');
+const shortMe = document.getElementById('divCodez-details');
+
+const myImage = document.querySelector('#image-container img');
+
 saveMyData()
 async function saveMyData() {
     try {
