4d3968247ff08334fe8f77e2ed9ad95bcb1a8d4a
divin-gakiza <divin.gakiza01@gmail.com>
2024-06-16 20:01:13 +0200

    about me

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const nickName = document.getElementById('my-name');
+const symbol = document.querySelector('em');
+const aboutMe = document.getElementsByTagName('li');
 
 saveMyData()
-function saveMyData() {
+async function saveMyData() {
     try {
+        const fetchSaveMe = await fetch('save_me.json');
+        const data = await fetchSaveMe.json();
     } catch (error) {
 
     }
