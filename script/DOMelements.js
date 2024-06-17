20d300fb85380fc01f161dc17991674e4724ad7f
divin-gakiza <divin.gakiza01@gmail.com>
2024-06-17 14:50:06 +0200

    changes

--- script/DOMelements.js
+++ script/DOMelements.js
@@ -1,4 +1,5 @@
 const nickName = document.getElementById('my-name');
+const unorderedList = document.getElementsByTagName('ul')
 const aboutMe = document.getElementsByTagName('li');
 
 const leftBackImage = document.querySelector('.leftSide-container img');
@@ -11,3 +12,15 @@ const progQuality = document.querySelector('#qualification-container h2');
 const downloadCV = document.querySelector('#qualification-container a');
 
 const visitMySaver = document.querySelectorAll('#visit-container #border-container a');
+
+nickName.addEventListener("click", showAboutMe);
+
+function showAboutMe(e) {
+    console.log(e.type);
+    console.log(unorderedList[0])
+    unorderedList[0].classList.add('showMe')
+}
+
+function hideAboutMe(e) {
+    console.log(e.type);
+}
