cb16a507080e92f08cfb88bd2af1b6f49fef7529
divin-gakiza <divin.gakiza01@gmail.com>
2024-06-17 04:23:00 +0200

    style all

--- script/downloadCV.js
+++ script/downloadCV.js
@@ -1,17 +1,16 @@
 function downloadCvHover() {
     const hoverSenser = document.querySelector('#qualification-container a #hover-me');
 
-    downloadCV.addEventListener('mouseover', function (ev) {
-
+    function hoverDownload(ev) {
         hoverSenser.classList.add('download');
-        hoverSenser.style.left = `${ev.offsetX - 20}px`
+        hoverSenser.style.left = `${ev.offsetX}px`
         hoverSenser.style.top = `${ev.offsetY + 20}px`
         console.log("jojo")
-    })
+    }
 
-    downloadCV.addEventListener('mouseleave', function (ev) {
+    function leaveDownload(ev) {
         hoverSenser.classList.remove('download');
         hoverSenser.style.left = `${ev.offsetX - 100}px`
         hoverSenser.style.top = `${ev.offsetY + 100}px`
-    })
+    }
 }
