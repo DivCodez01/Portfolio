95c0deef8530415ef5c0cd6ab63a363f808dfddf
divin-gakiza <divin.gakiza01@gmail.com>
2024-06-17 17:05:43 +0200

    auto delete

--- script/typeWriterProgLang.js
+++ script/typeWriterProgLang.js
@@ -11,3 +11,15 @@ const autotyping = (txt, incrm, func) => {
         }, 1000);
     }
 }
+
+const autoDeleting = (txt, incrm, func) => {
+    if (incrm >= 0) {
+        typeWriterElms.innerHTML = txt.substring(0, incrm--);
+        let typeElmTm = 10 + Math.random() * 10;
+        setTimeout(() => {
+            autoDeleting(txt, incrm, func);
+        }, typeElmTm);
+    } else if (typeof func == "function") {
+        setTimeout(func, 1000);
+    }
+}
